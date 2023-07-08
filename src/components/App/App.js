cba6d04c29cbb557929061de257f91eab043d36a
Sergei Pankov <sergeipankov98@mail.ru>
2023-07-08 22:30:51 +0400

    New version public test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,52 @@
+import { Routes, Route, Navigate, useNavigate, json } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 import { Header } from "../Header/Header";
 import { Main } from "../Main/Main";
 import { Footer } from "../Footer/Footer";
 
 function App() {
+
+  const [curretGame, setCurretGame] = useState("ForwardDungeon");
+
+  function changeCurretGame (game) {
+    setCurretGame(game);
+  }
+
   return (
-    <div className="body">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Routes>
+      <Route
+        path="/forwardungeon"
+        element={
+          <div className="body">
+            <Header curretGame={curretGame}/>
+            <Main />
+            <Footer />
+          </div>
+        }
+      />
+      <Route
+        path="/arena"
+        element={
+          <div className="body">
+            <Header curretGame={curretGame}/>
+            <Main />
+            <Footer />
+          </div>
+        }
+      />
+      <Route
+        path="/bunker"
+        element={
+          <div className="body">
+            <Header curretGame={curretGame}/>
+            <Main />
+            <Footer />
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,22 @@ import './header.css';
 
 export function Header(props) {
 
+  const [currentUrl, setCurrentUrl] = useState('');
+  useEffect(() => {
+      // Обновляем значение currentUrl при загрузке и изменении URL
+      setCurrentUrl(window.location.pathname);
+    }, []);
+
+
   return (
       <header className="header">
-        <h1 className='header__title'>ForwarDungeon</h1>
-        <a className='header__download-button' href={gameBuild} download={true}>Download</a>
+        <div className='header__games'>
+          <a href='arena' className={currentUrl === "/arena" ? "header__game header__game_selected" : "header__game"}>Arena</a>
+          <a href='forwardungeon' className={currentUrl === "/forwardungeon" ? "header__game header__game_selected" : "header__game"}>ForwarDungeon</a>
+          <a href='bunker' className={currentUrl === "/bunker" ? "header__game header__game_selected" : "header__game"}>Bunker</a>
+        </div>
+       
+        <a className='header__download-button' href={gameBuild} download={true}>Download {currentUrl.slice(1)}</a>
       </header>
   );
 }
