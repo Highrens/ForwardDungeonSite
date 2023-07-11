f95ac13014a35174820081d0d8c26be76e0c40c3
Sergei Pankov <sergeipankov98@mail.ru>
2023-07-11 15:54:48 +0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,9 +14,9 @@ export function Header(props) {
   return (
       <header className="header">
         <div className='header__games'>
-          <a href='/#/arena' className={currentUrl === "/arena" ? "header__game header__game_selected" : "header__game"}>Arena</a>
-          <a href='forwardungeon' className={currentUrl === "/" ? "header__game header__game_selected" : "header__game"}>ForwarDungeon</a>
-          <a href='#/bunker' className={currentUrl === "/bunker" ? "header__game header__game_selected" : "header__game"}>Bunker</a>
+          <a href='#/arena' className={currentUrl === "#/arena" ? "header__game header__game_selected" : "header__game"}>Arena</a>
+          <a href='#/' className={currentUrl === "#/" ? "header__game header__game_selected" : "header__game"}>ForwarDungeon</a>
+          <a href='#/bunker' className={currentUrl === "#/bunker" ? "header__game header__game_selected" : "header__game"}>Bunker</a>
         </div>
         {currentUrl === "/arena" ?
          <a className='header__download-button' href={ArenaBuild} download={true}  >Download {currentUrl.slice(1)}</a> : ""}
